1c6d70f35f04047c6c08a5cb4582c77df79b34cb
GuyCui <18024642@qq.com>
2020-11-05 21:01:59 +0800

    feat:
    修改输出换行问题

--- src/test/java/genericReflection/GenericReflectionTest.java
+++ src/test/java/genericReflection/GenericReflectionTest.java
@@ -27,11 +27,11 @@ public class GenericReflectionTest {
   }
 
   public static void printClass(Class<?> cl) {
-    System.out.println(cl);
+    System.out.print(cl);
     printTypes(cl.getTypeParameters(),"<",", ",">",true);
     Type sc = cl.getGenericSuperclass();
     if (sc != null) {
-      System.out.println(" extends ");
+      System.out.print(" extends ");
       printType(sc,false);
     }
     printTypes(cl.getInterfaces(), " implements ", ", ","",false);
@@ -40,14 +40,14 @@ public class GenericReflectionTest {
 
   public static void printMethod(Method m) {
     String name = m.getName();
-    System.out.println(Modifier.toString(m.getModifiers()));
-    System.out.println(" ");
+    System.out.print(Modifier.toString(m.getModifiers()));
+    System.out.print(" ");
     printTypes(m.getTypeParameters(),"<",", ","> ",true);
 
     printType(m.getGenericReturnType(),false);
-    System.out.println(" ");
-    System.out.println(name);
-    System.out.println("(");
+    System.out.print(" ");
+    System.out.print(name);
+    System.out.print("(");
     printTypes(m.getGenericParameterTypes(),"",", ","",false);
     System.out.println(")");
   }
@@ -58,32 +58,32 @@ public class GenericReflectionTest {
       return;
     }
     if (types.length > 0) {
-      System.out.println(pre);
+      System.out.print(pre);
     }
     for (int i = 0; i < types.length; i++) {
       if (i > 0) {
-        System.out.println(sep);
+        System.out.print(sep);
       }
       printType(types[i], isDefinition);
     }
     if (types.length > 0) {
-      System.out.println(suf);
+      System.out.print(suf);
     }
   }
 
   public static void printType(Type type, boolean isDefinition) {
     if (type instanceof Class) {
       Class<?> t = (Class<?>) type;
-      System.out.println(t.getName());
+      System.out.print(t.getName());
     } else if (type instanceof TypeVariable) {
       TypeVariable<?> t = (TypeVariable<?>) type;
-      System.out.println(t.getName());
+      System.out.print(t.getName());
       if (isDefinition) {
         printTypes(t.getBounds(), " extends ", " & ", " ", false);
       }
     } else if (type instanceof WildcardType) {
       WildcardType t = (WildcardType) type;
-      System.out.println("?");
+      System.out.print("?");
       printTypes(t.getUpperBounds(), " extends ", " & ", " ", false);
       printTypes(t.getUpperBounds(), " super ", " & ", " ", false);
     } else if (type instanceof ParameterizedType) {
@@ -91,15 +91,15 @@ public class GenericReflectionTest {
       Type owner = t.getOwnerType();
       if (owner != null) {
         printType(owner, false);
-        System.out.println(".");
+        System.out.print(".");
       }
       printType(t.getRawType(), false);
       printTypes(t.getActualTypeArguments(), "<", ", ", ">", false);
     } else if (type instanceof GenericArrayType) {
       GenericArrayType t = (GenericArrayType) type;
-      System.out.println("");
+      System.out.print(" ");
       printType(t.getGenericComponentType(), isDefinition);
-      System.out.println("[]");
+      System.out.print("[]");
     }
   }
 }
