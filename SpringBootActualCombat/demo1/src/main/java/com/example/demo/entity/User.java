de4ab1b3a84228ffb6d9ee6f1654cc38963e12f4
GuyCui <18024642@qq.com>
2020-11-25 23:00:09 +0800

    Spring Boot 实战派 -新增 验证表单数据并实现数据的自定义验证~

---
+++ SpringBootActualCombat/demo1/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import com.example.demo.annotation.MyCustomConstraint;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+
+/**
+ * @author guycui
+ */
+@Data
+public class User implements Serializable {
+	private Long id;
+
+	@NotBlank(message = "用户名不能为空")
+	@Length(min = 5,max = 20,message = "用户名长度 5-20 个字符")
+	private String name;
+
+	@NotNull(message = "年龄不能为空")
+	@Min(value = 18,message = "最小 18 岁")
+	@Max(value = 60,message = "最大 60 岁")
+	private Integer age;
+
+	@Email(message = "请输入邮箱")
+	@NotBlank(message = "邮箱不能为空")
+	private String email;
+
+	@MyCustomConstraint
+	private String answer;
+}
