8d8cf70d45676b2b9819f7f5feff5fa135d719b7
GuyCui <18024642@qq.com>
2020-11-26 21:57:29 +0800

    Spring Boot 实战派 -新增 WebFlux学习

---
+++ SpringBootActualCombat/demo1/webflux/src/main/java/com/example/webFlux/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.webFlux.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @FileName：HelloWorldController @Author：guycui @Date：2020/11/26 下午9:43 @Description：WebFlux 控制器
+ */
+@RestController
+public class HelloWorldController {
+  @GetMapping("/hello")
+  public Mono<String> helloFlux() {
+    return Mono.just("This is WebFlux demo");
+  }
+}
