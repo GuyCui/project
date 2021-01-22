2271b149c60ef7e2f5f8235f078c92e4340da975
崔明志 <18024642@qq.com>
2021-01-22 08:40:12 +0800

    结合 ExtJS 进行配置修改

--- webshell/extJs/WebShell/app/view/login/LoginController.js
+++ webshell/extJs/WebShell/app/view/login/LoginController.js
@@ -4,22 +4,23 @@ Ext.define('WebShell.view.login.LoginController', {
 
     onLoginClick: function() {
         Ext.Ajax.request({
-                // 被用来向服务器发起请求默认的url
-                url: "http://localhost:8080/webShell",
-                // 请求成功时回调函数
-                success: function () {
-                    localStorage.setItem("TutorialLoggedIn", true);
+            // 被用来向服务器发起请求默认的url
+            url: "http://localhost:8081/webShell",
+            // 请求成功时回调函数
+            success: function (ehr) {
+                console.log("ehr", ehr)
+                localStorage.setItem("TutorialLoggedIn", true);
 
-                    this.getView().destroy();
+                this.getView().destroy();
 
-                    Ext.create({
-                        xtype: 'app-main'
-                    });
-                },
-                // 请求失败时回调函数
-                /*failure: function () {
-                    Ext.ux.Toast.msg("信息提示", "信息出错，请重新输入!");
-                }*/
+                Ext.create({
+                    xtype: 'app-main'
+                });
+            },
+            // 请求失败时回调函数
+            /*failure: function () {
+                Ext.ux.Toast.msg("信息提示", "信息出错，请重新输入!");
+            }*/
             }
         );
 
